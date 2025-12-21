df532d3e1d129935365351cd8cf6876ca48e7b69
Dhananjay Badoni <101263267+Dhananjay0719@users.noreply.github.com>
2025-12-21 18:47:15 +0530

    Implement SodaPrice contract with price management

--- sodaprice.sol
+++ sodaprice.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "./interfaces.sol";
+
+contract SodaPrice is ISodaPrice {
+    uint private price;
+    address public owner;
+    constructor(uint _initialprice){
+        owner=msg.sender;
+        price=_initialprice;
+    }
+
+    function getPrice() external view returns(uint){
+        return price;
+    }
+
+    function setPrice(uint _price) public {
+        require(owner==msg.sender,"Only Owner Can Set Price");
+        price=_price;
+    }
+}
